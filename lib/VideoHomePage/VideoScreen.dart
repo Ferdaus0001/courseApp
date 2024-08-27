b9d8b96c38cf94c2bbf6cbbd8339398e7a257aa8
Ferdaus0001 <ferdaushossan7788990@gmail.com>
2024-08-27 23:02:31 +0600

    add Containte  Image

--- lib/VideoHomePage/VideoScreen.dart
+++ lib/VideoHomePage/VideoScreen.dart
@@ -20,35 +20,42 @@ class VideoScreen extends StatelessWidget {
                   children: [
 
                     Container(
+                  
                       height: 200,
                       width: 200,
                       color: Colors.purple,
-                      child: Icon(Icons.person,size: 66,),
+                      child: Image(image: AssetImage('assets/Image/AzidVai.jpg'),fit: BoxFit.cover,),
 
                     ),
                     SizedBox(width: 5,),
                     Container(
+                
                       height: 200,
                       width: 200,
                       color: Colors.red,
+                      child: Image(image: NetworkImage('assets/Image/Screenshot 2024-08-27 224006.png'),fit: BoxFit.fill,),
                     ),
                     SizedBox(width: 5,),
                     Container(
+                    
                       height: 200,
                       width: 200,
                       color: Colors.green,
+                      child: Image(image: NetworkImage('assets/Image/WebDeveloper.jpg'),fit: BoxFit.fill,),
                     ),
                     SizedBox(width: 5,),
                     Container(
                       height: 200,
                       width: 200,
                       color: Colors.orange,
+                      child: Image(image: NetworkImage('assets/Image/PythonSir.jpg')),
                     ),
                     SizedBox(width: 5,),
                     Container(
                       height: 200,
                       width: 200,
                       color: Colors.pink,
+                      child: Image(image: NetworkImage('assets/Image/Networksir.jpg')),
                     ),
                   ],
 
@@ -65,15 +72,63 @@ class VideoScreen extends StatelessWidget {
               height: 200,
               width: double.infinity,
               color: Colors.blue,
+              child: Image(image: NetworkImage('assets/Image/course.jpg'),fit: BoxFit.cover,),
+              padding: EdgeInsets.all(3),
             ),
             SizedBox(height: 7,),
             Container(
+              padding: EdgeInsets.all(3),
+              height: 200,
+              width: double.infinity,
+              color: Colors.blue,
+              child: Image(image: NetworkImage('assets/Image/2885309.jpg'),fit: BoxFit.cover,),
+            ),
+            SizedBox(height: 8,),
+            Container(
+              padding: EdgeInsets.all(3),
+              child: Image(image: NetworkImage('assets/Image/postman_online_4.jpg'),fit: BoxFit.cover,),
               height: 200,
               width: double.infinity,
               color: Colors.blue,
             ),
             SizedBox(height: 8,),
             Container(
+              padding: EdgeInsets.all(3),
+              child: Image(image: NetworkImage('assets/Image/postman_online_4.jpg'),fit: BoxFit.cover,),
+              height: 200,
+              width: double.infinity,
+              color: Colors.blue,
+            ),
+            SizedBox(height: 8,),
+            Container(
+              padding: EdgeInsets.all(3),
+              child: Image(image: NetworkImage('assets/Image/woman.jpg'),fit: BoxFit.cover,),
+              height: 200,
+              width: double.infinity,
+              color: Colors.blue,
+            ),
+
+            SizedBox(height: 8,),
+            Container(
+              padding: EdgeInsets.all(3),
+              child: Image(image: NetworkImage('assets/Image/postman_online_4.jpg'),fit: BoxFit.cover,),
+              height: 200,
+              width: double.infinity,
+              color: Colors.blue,
+            ),
+            SizedBox(height: 8,),
+            Container(
+              padding: EdgeInsets.all(3),
+              child: Image(image: NetworkImage('assets/Image/boy.jpg'),fit: BoxFit.cover,),
+              height: 200,
+              width: double.infinity,
+              color: Colors.blue,
+            ),
+
+            SizedBox(height: 8,),
+            Container(
+              padding: EdgeInsets.all(3),
+              child: Image(image: NetworkImage('assets/Image/job.jpg'),fit: BoxFit.cover,),
               height: 200,
               width: double.infinity,
               color: Colors.blue,
