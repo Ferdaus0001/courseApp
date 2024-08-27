dc5cc6c04deb95b8cb9cdad9295e3aab19f2573b
Ferdaus0001 <ferdaushossan7788990@gmail.com>
2024-08-27 12:36:16 +0600

    Digine  Accountcreens

--- lib/RountPage/AccountScreen.dart
+++ lib/RountPage/AccountScreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:salomon_bottom_bar/salomon_bottom_bar.dart';
 
 class AccountScreen extends StatelessWidget {
@@ -9,10 +10,87 @@ class AccountScreen extends StatelessWidget {
     return Scaffold(
 
 
-    body: Center(
-      child: Container(
-        child: Text('Account ',style: TextStyle(fontSize: 33,fontWeight: FontWeight.bold,color: Colors.black),),
-        color: Colors.green,
+    body:  Padding(
+      padding: const EdgeInsets.all(7.0),
+      child: Column(
+
+        children: [
+          SizedBox(height: 2,),
+          const CircleAvatar(
+          radius: 70,
+            backgroundImage: NetworkImage('assets/Image/ferdaus.jpg'),
+          ),
+          SizedBox(height: 2,),
+          Card(
+            child: InkWell(
+              onTap: (){},
+              child: ListTile(
+                selectedColor: Colors.blue,
+                focusColor: Colors.blueAccent,
+                title: Text('Name '),
+                subtitle: Text('Ferdaus Hossan '),
+                leading: Icon(Icons.person),
+                trailing: Icon(Icons.arrow_forward,color: Colors.blue,),
+              ),
+            ),
+          ),
+         /// Card Two and ListTile Two
+          SizedBox(height:2,),
+          Card(
+            child: InkWell(
+              onTap: (){},
+              child: ListTile(
+                focusColor: Colors.blueAccent,
+                title: Text(' Phone Number  '),
+                subtitle: Text(' 01726176754 '),
+                leading: Icon(Icons.phone_in_talk_sharp),
+                trailing: Icon(Icons.arrow_forward,color: Colors.blue,),
+              ),
+            ),
+          ),
+          // Card Three and ListTile Three
+          SizedBox(height: 2,),
+          Card(
+            child: InkWell(
+              onTap: (){},
+              child: ListTile(
+                focusColor: Colors.blueAccent,
+                title: Text('Email  '),
+                subtitle: Text('FerdausHossan7788990@gmail.com '),
+                leading: Icon(Icons.email_outlined),
+                trailing: Icon(Icons.arrow_forward,color: Colors.blue,),
+              ),
+            ),
+          ),
+          SizedBox(height: 2,),
+          Card(
+            child: InkWell(
+              onTap: (){},
+              child: ListTile(
+                focusColor: Colors.blueAccent,
+                title: Text('Adress '),
+                subtitle: Text('Bangladesh Dhaka Jessore '),
+                leading: Icon(Icons.add_home_work_outlined),
+                trailing: Icon(Icons.arrow_forward,color: Colors.blue,),
+              ),
+            ),
+          ),
+          SizedBox(height: 2,),
+          ElevatedButton(onPressed: (){
+
+            Get.snackbar('Massage ', 'Edit Complete  Sir ');
+
+
+          },
+              child: Text('Edite  Profile ',style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold,color: Colors.blue,),),
+            style: ElevatedButton.styleFrom(
+              padding: EdgeInsets.all(11),
+
+            ),
+          ),
+
+        ],
+
       ),
     ),
 
